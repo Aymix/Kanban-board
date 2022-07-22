036c0a30687ed374372fdce7428cb6c72e0f5370
Aymen Hmida <aymenhmida1@gmail.com>
2022-07-22 18:55:24 +0100

    1st commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <v-app>
+            <v-container>
+                <GetTasks></GetTasks>
+                <v-row>
+                <CurrentTasks></CurrentTasks>
+                <CompletedTasks></CompletedTasks>
+              </v-row>
+            </v-container>
+        </v-app>
+    </div>
+</template>
+<script>
+import GetTasks from './parts/GetTasks.vue'
+import CurrentTasks from './parts/CurrentTasks.vue'
+import CompletedTasks from './parts/CompletedTasks.vue'
+export default {
+    name: 'HelloWorld',
+
+    components: {
+        GetTasks,
+        CurrentTasks,
+        CompletedTasks
+    },
+
+    watch: {
+      '$store.state.todos': function() {
+     localStorage.setItem('todos', JSON.stringify(this.$store.state.todos));
+  }
+  }
+}
+
+</script>
